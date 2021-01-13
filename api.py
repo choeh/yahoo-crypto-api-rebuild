d4304919faeec58fb68a2c146818a8214fdacd98
choeh <75442468+choeh@users.noreply.github.com>
2021-01-13 15:09:45 +0100

    Update scraper loading in backend api

--- api.py
+++ api.py
@@ -3,8 +3,6 @@ import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from fake_useragent import UserAgent
-from fp.fp import FreeProxy
 
 from scraper import init_request_args
 
@@ -32,21 +30,22 @@ uri = '/cryptocurrencies/'
 
 url = f'{domain}{uri}'
 
-# Load stored scraper
 scraper = AutoScraper()
-scraper.load('yahoo_crypto')
-request_args = init_request_args(headers=scraper.request_headers, url=url)
+request_args = init_request_args(url=url)
 
 
 def get_yahoo_crypto_data():
+    # Load stored scraper
+    scraper.load('yahoo_crypto_data.json')
+
     # Scrape table entries from all subpages
-    data = pd.DataFrame([], columns=['Symbol', 'Name', 'Price', 'Logo', 'MarketCap'])
+    data = pd.DataFrame([], columns=['Symbol', 'Name', 'Logo', 'Price', 'Change_Relative', 'Marketcap', 'Volume_24H', 'Supply_Circulating'])
     n_page = 0
     rows_per_page = 100
     while data.shape[0] % rows_per_page == 0 and n_page < 10:
-        url = f'{domain}{uri}?offset={n_page*rows_per_page}&count={rows_per_page}'
+        url_paged_query = f'{url}?offset={n_page*rows_per_page}&count={rows_per_page}'
         try:
-            ret = scraper.get_result_similar(url, group_by_alias=True, keep_order=True, request_args=request_args)
+            ret = scraper.get_result_similar(url_paged_query, group_by_alias=True, keep_order=True, request_args=request_args)
             ret['Logo'] = ret['Logo'] + [''] * (len(ret['Symbol']) - len(ret['Logo']))  # TODO: temp handling of none in logo
             data = pd.concat([data, pd.DataFrame(ret)], ignore_index=True)
             n_page += 1
@@ -56,8 +55,10 @@ def get_yahoo_crypto_data():
 
 
 def get_yahoo_crypto_news():
+    # Load stored scraper
+    scraper.load('yahoo_crypto_news.json')
+
     # Scrape news from page
-    url = f'{domain}{uri}'
     ret = scraper.get_result_similar(url, group_by_alias=True, keep_order=True, request_args=request_args)
     data = pd.DataFrame(ret, columns=['News', 'Urls'])
     return data
